ea5aaa6c2044126702638215a2484542542fdc05
jimtahu <jimtahu@gmail.com>
2016-11-26 22:49:55 -0600

    Moved quad data

--- brain.js
+++ brain.js
@@ -4,35 +4,8 @@ var Neuron = synaptic.Neuron,
     Network = synaptic.Network,
     Trainer = synaptic.Trainer,
     Architect = synaptic.Architect;
-
-var data=[
-    { input: [ 0, 0, 0, 0 ], output: [ 0, 0 ] },
-    { input: [ 0, 0, 0, 1 ], output: [ 0, 1 ] },
-    { input: [ 0, 0, 1, 0 ], output: [ 0, 1 ] },
-    { input: [ 0, 0, 1, 1 ], output: [ 0, 0 ] },
-    { input: [ 0, 1, 0, 0 ], output: [ 1, 0 ] },
-    { input: [ 0, 1, 0, 1 ], output: [ 1, 1 ] },
-    { input: [ 0, 1, 1, 0 ], output: [ 1, 1 ] },
-    { input: [ 0, 1, 1, 1 ], output: [ 1, 0 ] },
-    { input: [ 1, 0, 0, 0 ], output: [ 1, 0 ] },
-    { input: [ 1, 0, 0, 1 ], output: [ 1, 1 ] },
-    { input: [ 1, 0, 1, 0 ], output: [ 1, 1 ] },
-    { input: [ 1, 0, 1, 1 ], output: [ 1, 0 ] },
-    { input: [ 1, 1, 0, 0 ], output: [ 0, 0 ] },
-    { input: [ 1, 1, 0, 1 ], output: [ 0, 1 ] },
-    { input: [ 1, 1, 1, 0 ], output: [ 0, 1 ] },
-    { input: [ 1, 1, 1, 1 ], output: [ 0, 0 ] }
-    ];
-
-var defaults = {
-        iterations: 100000,
-        log: false,
-        shuffle: true,
-        cost: Trainer.cost.MSE
-    };
-var myPer = new Architect.Perceptron( 4, 5, 2 );
-var myTrain = new Trainer( myPer );
-myTrain.train( data, defaults );
+var quadzor = require('./quadzor');
+var myPer = quadzor.makeQuad( );
 
 console.log(myPer.activate([0,0,0,0]));
 console.log(myPer.activate([1,0,1,0]));
---
+++ quadzor.js
@@ -0,0 +1,40 @@
+var synaptic = require('synaptic');
+var Neuron = synaptic.Neuron,
+    Layer = synaptic.Layer,
+    Network = synaptic.Network,
+    Trainer = synaptic.Trainer,
+    Architect = synaptic.Architect;
+
+function makeQuad( ){
+    var data=[
+        { input: [ 0, 0, 0, 0 ], output: [ 0, 0 ] },
+        { input: [ 0, 0, 0, 1 ], output: [ 0, 1 ] },
+        { input: [ 0, 0, 1, 0 ], output: [ 0, 1 ] },
+        { input: [ 0, 0, 1, 1 ], output: [ 0, 0 ] },
+        { input: [ 0, 1, 0, 0 ], output: [ 1, 0 ] },
+        { input: [ 0, 1, 0, 1 ], output: [ 1, 1 ] },
+        { input: [ 0, 1, 1, 0 ], output: [ 1, 1 ] },
+        { input: [ 0, 1, 1, 1 ], output: [ 1, 0 ] },
+        { input: [ 1, 0, 0, 0 ], output: [ 1, 0 ] },
+        { input: [ 1, 0, 0, 1 ], output: [ 1, 1 ] },
+        { input: [ 1, 0, 1, 0 ], output: [ 1, 1 ] },
+        { input: [ 1, 0, 1, 1 ], output: [ 1, 0 ] },
+        { input: [ 1, 1, 0, 0 ], output: [ 0, 0 ] },
+        { input: [ 1, 1, 0, 1 ], output: [ 0, 1 ] },
+        { input: [ 1, 1, 1, 0 ], output: [ 0, 1 ] },
+        { input: [ 1, 1, 1, 1 ], output: [ 0, 0 ] }
+    ];
+
+    var defaults = {
+            iterations: 100000,
+            log: false,
+            shuffle: true,
+            cost: Trainer.cost.MSE
+        };
+    var myPer = new Architect.Perceptron( 4, 5, 2 );
+    var myTrain = new Trainer( myPer );
+    myTrain.train( data, defaults );
+    return myPer;
+};
+
+exports.makeQuad = makeQuad;
